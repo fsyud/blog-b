8be7b909d11a98d980d5e2e3fc633b8450611c57
dongyu <naze.dong@soundws.com>
2021-06-23 19:28:30 +0800

    feat: 项目初始化

---
+++ config/routes.ts
@@ -0,0 +1,66 @@
+﻿export default [
+  {
+    path: '/user',
+    layout: false,
+    routes: [
+      {
+        path: '/user',
+        routes: [
+          {
+            name: 'login',
+            path: '/user/login',
+            component: './user/Login',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    path: '/welcome',
+    name: 'welcome',
+    icon: 'smile',
+    component: './Welcome',
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    icon: 'crown',
+    access: 'canAdmin',
+    component: './Admin',
+    routes: [
+      {
+        path: '/admin/sub-page',
+        name: 'sub-page',
+        icon: 'smile',
+        component: './Welcome',
+      },
+    ],
+  },
+  {
+    name: 'list.table-list',
+    icon: 'table',
+    path: '/list',
+    component: './TableList',
+  },
+  {
+    path: '/article',
+    name: 'article',
+    icon: 'smile',
+    component: './Article',
+    routes: [
+      {
+        path: '/article/create',
+        name: 'article-create',
+        icon: 'smile',
+        component: './Article',
+      },
+    ],
+  },
+  {
+    path: '/',
+    redirect: '/welcome',
+  },
+  {
+    component: './404',
+  },
+];
---
+++ src/pages/Article/index.tsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import { Card } from 'antd';
+import BraftEditor from 'braft-editor';
+import { Drawer, Button } from 'antd';
+import 'braft-editor/dist/index.css';
+
+import styles from './index.less';
+
+const Article: React.FC = () => {
+  const [value, setValue] = useState<any>(
+    BraftEditor.createEditorState('<p>nice <b>day!</b></p>'),
+  );
+  const [visible, setVisible] = useState<boolean>(false);
+
+  // 预览
+  const preview = () => {
+    setVisible(true);
+  };
+
+  const extendControls: any = [
+    {
+      key: 'custom-button',
+      type: 'button',
+      text: '预览',
+      onClick: preview,
+    },
+  ];
+
+  const submit = async (): Promise<any> => {
+
+  }
+
+  return (
+    <div className={styles.creat_article}>
+      <Card>
+        <div className={styles.header}>
+          <Button type="primary" size="middle" onClick={submit}>
+            提交
+          </Button>
+        </div>
+        <BraftEditor
+          value={value}
+          onChange={(val: any) => {
+            setValue(val);
+          }}
+          extendControls={extendControls}
+        />
+        
+        <Drawer
+          title="预览"
+          placement="right"
+          closable={false}
+          width={1000}
+          onClose={() => setVisible(false)}
+          visible={visible}
+        >
+          <div
+            className={styles.toc_index}
+            dangerouslySetInnerHTML={{
+              __html: value.toHTML(),
+            }}
+          />
+        </Drawer>
+
+      </Card>
+    </div>
+  );
+};
+
+export default Article;
